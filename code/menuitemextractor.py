a61b3cf1a82d3a556256d17ab3c46033bee12386
avallenn <avallen@syr.edu>
2024-11-20 11:03:42 -0500

    functions

--- code/menuitemextractor.py
+++ code/menuitemextractor.py
@@ -7,13 +7,38 @@ else:
 
 
 def clean_price(price:str) -> float:
-    pass # TODO  replace with code
+    # $9.99 -> 9.99 float
+    price = price.replace('$','')
+    price = price.replace(',','')
+    return float(price)
 
 def clean_scraped_text(scraped_text: str) -> list[str]:
-    pass # TODO  replace with code
+    # remove empty lines, lines that are "NEW!" or "NEW", lines that say "S", "V", "GS", "GF", or "P"
+    # split the text on a newline
+    # return the list of lines
+    texts = scraped_text.split('\n')
+    cleaned_text = []
+    for text in texts:
+        if text in ['GS',"V","S","P"]:
+            continue
+        if text.startswith("NEW"):
+            continue
+        if len(text.strip()) == 0:
+            continue
+
+        cleaned_text.append(text)
+    return cleaned_text
 
 def extract_menu_item(title:str, scraped_text: str) -> MenuItem:
-    pass # TODO  replace with code
+    cleaned_text = clean_scraped_text(scraped_text)
+    name = cleaned_text[0]
+    price = clean_price(cleaned_text[1])
+    if len(cleaned_text) > 2:
+        description = cleaned_text[2]
+    if len(cleaned_text) == 2:
+        description = 'No description available.'
+    item = MenuItem(name=name, price=price, category=title, description=description)
+    return item
 
 
 
